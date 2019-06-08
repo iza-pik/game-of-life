ae6e144c44575487d3194056b619a7f26587a67c
Izabela <i.j.pikula@gmail.com>
2019-06-08 14:22:37 +0100

    Updates

---
+++ precache-manifest.c8861317763af7b6a59676b2eebc69fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaa9b95640024ac2657a7281ab9e242",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "d686520ff4ef0d64a56d",
+    "url": "/game-of-life/static/css/main.a55688e5.chunk.css"
+  },
+  {
+    "revision": "9ced0f3dcb08e9836ab0",
+    "url": "/game-of-life/static/js/2.8b3356a5.chunk.js"
+  },
+  {
+    "revision": "d686520ff4ef0d64a56d",
+    "url": "/game-of-life/static/js/main.6ca00fd0.chunk.js"
+  },
+  {
+    "revision": "b485c445b65ba023756b",
+    "url": "/game-of-life/static/js/runtime~main.a7312194.js"
+  }
+]);
